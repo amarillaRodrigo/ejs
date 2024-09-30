272ffe8e938a8bb3195229d32e697ce7bf7db86b
Rodrigo Amarilla <amarillarodrigo14@gmail.com>
2024-09-30 16:25:18 -0300

    Actualizacion

--- src/server.js
+++ src/server.js
@@ -1,34 +1,69 @@
 import express from "express";
 import morgan from "morgan";
 
+
+//Settings
+app.set('appName', 'Express Course')
+
+//Middlewares
 const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
 
-const products = {
+
+let products = {
   id: 1,
   nombre: "Mayonesa",
   price: 3000,
 };
+
+
+//Routes
 
 app.get("/products", (req, res) => {
   res.send("Obteniendo productos");
 });
 
 app.get("/products/:id", (req, res) => {
-  res.send("Obteniendo producto por id");
+  console.log(req.params.id);
+  products.find(function (product) {
+    const productFound = product.find((p) => p.id == parseInt(req.params.id));
+    if (!productFound) {
+      return res.status(400).json({ message: "Product not found" });
+    }
+    res.send(productFound);
+  });
 });
 app.post("/products", (req, res) => {
-  console.log({...req.body, id: 3});
+  const newProduct = { ...req.body, id: products.lenght + 1 };
+  products.push(newProduct);
   res.send("Creando productos");
 });
+
 app.put("/products", (req, res) => {
-  res.send("Actualizando producto");
+  const newData = req.body
+  products.find(function (product) {
+    const productFound = product.find((p) => p.id == parseInt(req.params.id));
+    if (!productFound) {
+      return res.status(400).json({ message: "Product not found" });
+    }
+  });
+
+  products = products.map( p => p.id === parseInt(req.params.id) ? {...p, ...newData} : p)
+  res.json({message : "Producto actualizado"})
 });
+
 app.patch("/", (req, res) => {
   res.send("Actualizando caracteristica de un producto");
 });
-app.delete("/", (req, res) => {
+app.delete("/products/:id", (req, res) => {
+  const productFound = product.find((p) => p.id == parseInt(req.params.id));
+  if (!productFound) {
+    return res.status(400).json({ message: "Product not found" });
+  }
+
+  products = products.filter((p) => p.id !== parseInt(req.params.id));
+  console.log(newProduct);
   res.send("Borrando producto");
 });
 
